da76c31d155665a16acc38f9a9a8e5b5592cde5f
churihyung <dlwk0807@gmail.com>
2022-02-02 16:51:40 +0900

    how_to_fix_copy&paste_copyright

--- javascript.js
+++ javascript.js
@@ -12,9 +12,40 @@ for(const i of hellos){
 
 const find = document.querySelectorAll(".find-hello h2");
 
-function h1mouseOver(){
+function handleh1mouseOver(){
     h1Title.style.fontSize = "40px";
 }
 
-h1Title.addEventListener("click",()=>alert("bow!"));
-h1Title.addEventListener("mouseover",h1mouseOver);
+h1Title.addEventListener("click",()=>{
+                          console.log("click!!")});
+h1Title.addEventListener("mouseover",handleh1mouseOver);
+
+
+//window
+function handleWindowResize(){
+    document.body.style.backgroundColor="beige";
+}
+
+function handleWindowCopy(){
+    
+    // 읽기
+    navigator.clipboard.readText().then((text) => {
+    });
+}
+
+//copy & paste 복사붙여넣기 출처남기기  어떻게 수정해야되지 
+function handleWindowPaste(){
+    //쓰기 
+    let copyText;
+    navigator.clipboard.readText().then((text) => {
+        copyText = text+ "[churihyung]붙여넣기 테스트";
+
+        navigator.clipboard.writeText(copyText).then(() => {
+        });
+    });
+    
+}
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener("copy",handleWindowCopy, true);
+window.addEventListener("paste",handleWindowPaste,true);
+
