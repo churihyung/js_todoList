c488e9f41cd06a7cbe354015c500b71e97619007
churihyung <dlwk0807@gmail.com>
2022-02-02 17:34:08 +0900

    learn_about_toggle

--- javascript.js
+++ javascript.js
@@ -12,40 +12,69 @@ for(const i of hellos){
 
 const find = document.querySelectorAll(".find-hello h2");
 
-function handleh1mouseOver(){
-    h1Title.style.fontSize = "40px";
-}
 
-h1Title.addEventListener("click",()=>{
-                          console.log("click!!")});
-h1Title.addEventListener("mouseover",handleh1mouseOver);
+// //window
+// function handleWindowResize(){
+//     document.body.style.backgroundColor="beige";
+// }
 
-
-//window
-function handleWindowResize(){
-    document.body.style.backgroundColor="beige";
-}
-
-function handleWindowCopy(){
+// function handleWindowCopy(){
     
-    // 읽기
-    navigator.clipboard.readText().then((text) => {
-    });
-}
+//     // 읽기
+//     navigator.clipboard.readText().then((text) => {
+//     });
+// }
 
-//copy & paste 복사붙여넣기 출처남기기  어떻게 수정해야되지 
-function handleWindowPaste(){
-    //쓰기 
-    let copyText;
-    navigator.clipboard.readText().then((text) => {
-        copyText = text+ "[churihyung]붙여넣기 테스트";
+// //copy & paste 복사붙여넣기 출처남기기  어떻게 수정해야되지 
+// function handleWindowPaste(){
+//     //쓰기 
+//     let copyText;
+//     navigator.clipboard.readText().then((text) => {
+//         copyText = text+ "[churihyung]붙여넣기 테스트";
 
-        navigator.clipboard.writeText(copyText).then(() => {
-        });
-    });
+//         navigator.clipboard.writeText(copyText).then(() => {
+//         });
+//     });
     
+// }
+// window.addEventListener("resize",handleWindowResize);
+// window.addEventListener("copy",handleWindowCopy, true);
+// window.addEventListener("paste",handleWindowPaste,true);
+
+//h1 click 마다 color 바꾸기
+//방법 1.
+function handleH1BackColor(){
+    const backColor = h1Title.style.color;
+    let newColor
+    if(backColor == 'tomato'){
+        newColor = "teal";
+    }
+    else{
+        newColor = "tomato";
+    }
+    h1Title.style.color = newColor;
 }
-window.addEventListener("resize",handleWindowResize);
-window.addEventListener("copy",handleWindowCopy, true);
-window.addEventListener("paste",handleWindowPaste,true);
+
+
+//방법 2. javascript가 style에 관여를 적게
+function handleH1BackColor(){
+    // const clicked = "active"
+    // if(h1Title.classList.contains(clicked)){
+    //     h1Title.classList.remove(clicked);
+    // }
+    // else{
+    //     h1Title.classList.add(clicked);
+    // }
+
+    //위에 코드 한줄로 처리가능함 string이 중복되면 변수로 만들어 진행
+    h1Title.classList.toggle("active");
+}
+
+
+
+
+
+
+h1Title.addEventListener("click",handleH1BackColor);
+
 
