081970e55346ed055feb5834ae3bd4992a7ba40b
churihyung <dlwk0807@gmail.com>
2022-02-05 17:06:08 +0900

    file_manage_and_clock_done

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+const clockYear = document.querySelector("#clock-year");
+const clockTime = document.querySelector("#clock-time");
+
+function setClock(){
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2,'0');
+    const minutes = date.getMinutes().toString().padStart(2,'0');
+    const second = date.getSeconds().toString().padStart(2,'0');
+    clockYear.innerText = `${date.getFullYear()}년 ${date.getMonth()+1}월 ${date.getDate()}일`;
+    clockTime.innerText = `${hours}:${minutes}:${second}`;
+}
+setClock();
+setInterval(setClock,1000);
+
+
+// console.log(date.toLocaleString());
