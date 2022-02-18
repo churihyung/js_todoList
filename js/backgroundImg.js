3ae18ae0dede80dc3ffc80427e7fb554e80b90be
churihyung <dlwk0807@gmail.com>
2022-02-18 16:51:01 +0900

    fin

--- js/backgroundImg.js
+++ js/backgroundImg.js
@@ -1,8 +1,7 @@
-imgsArr = ["01.jpg","02.jpg","03.jpg","04.jpg"];
-const selectedImg = imgsArr[Math.floor(Math.random()*imgsArr.length)];
-
+imgsArr = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+const selectedImg = imgsArr[Math.floor(Math.random() * imgsArr.length)];
 
 const imgElement = document.createElement("img");
-imgElement.src=`background-img/${selectedImg}`;
+imgElement.src = `background-img/${selectedImg}`;
 
 document.body.appendChild(imgElement);
